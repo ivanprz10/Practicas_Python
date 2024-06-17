4d7a697a2761187c0ed990f21428346c83d88449
ivanprz10 <ivanpcruz07@gmail.com>
2024-06-17 17:11:14 -0600

    agregando input

---
+++ tuto_input.py
@@ -0,0 +1,11 @@
+# Leer una cadena de texto
+texto = input("Ingresa una cadena de texto: ")#guardamos en un input la cadena de texto que ingresemos por teclado
+print("Has ingresado la cadena:", texto)
+
+# Leer un número entero
+entero = int(input("Ingresa un número entero: "))#aqui es diferente almacenamos en un input lo que necesitemos pero, el resultado lo guardaremos en entero
+print("Has ingresado el número entero:", entero)
+
+# Leer un número flotante
+flotante = float(input("Ingresa un número flotante: "))#mismo caso pero en flotante
+print("Has ingresado el número flotante:", flotante) 
