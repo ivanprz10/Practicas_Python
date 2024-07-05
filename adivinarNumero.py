057d53f09421db51f285d1b6c8d12003ac01e89d
ivanprz10 <ivanpcruz07@gmail.com>
2024-07-04 23:24:18 -0600

    practicas

---
+++ adivinarNumero.py
@@ -0,0 +1,18 @@
+#adivinar un numero del rango del uno al 10 
+import random
+
+aleatorio = random.randint(1,10)
+
+print("numero secreto es: ",aleatorio)
+
+condicion = True
+
+while condicion == True:
+    numero = int(input("ingresa tu numero: "))
+
+    if numero == aleatorio:
+        print("FELICIDADES \nhas ganado")
+        condicion = False
+    else:
+        print("intentalo de nuevo ")
+
--- pruebas.py
+++ pruebas.py
@@ -1,20 +1,11 @@
 #USO SOLO PARA PRUEBAS DE CODIGO EN PYTHON
 #---------------------------------------------------------------------------------------------------------------
 
-import random
+#crea un bucle infinito
 
-aleatorio = random.randint(1,10)
+condicion = True
+contador = 0
+while condicion == True:
+    contador = contador + 1
+    print(contador)
 
-print(aleatorio)
-
-acierto = True
-print("el numero secreto es: ",aleatorio)
-
-while acierto == True:
-    n = int(input("ingrese un numero aleatorio entre uno y 10: "))
-
-    if n == aleatorio:
-        print("le atinaste, felicidades:)")
-        acierto = False
-    else:
-        print("fallaste, intentalo de nuevo")
