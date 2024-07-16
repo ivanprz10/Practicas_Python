70d841e69e227e154378ccee61ccaf07128ca530
ivanprz10 <ivanpcruz07@gmail.com>
2024-07-16 16:52:13 -0600

    Array unidimensional

--- pruebas.py
+++ pruebas.py
@@ -1,20 +1,22 @@
 #USO SOLO PARA PRUEBAS DE CODIGO EN PYTHON
 #---------------------------------------------------------------------------------------------------------------
-
 class automovil:
-    llantas=4
-    def __init__(self, marca,vel):
+    numLlantas=4
+    espejos =2
+    def __init__(self, marca, vel):
+        
         self.marca=marca
         self.velMax=vel
-
     def avanzar(self):
-        print(f"el automovil '{self.marca}' avanzo con una velocidad inicial de: {self.velMax/12} km/hr")
-
-carro1=automovil("audi",300)
-
-carro1.avanzar()
+        print(f"el carro {self.marca}, inicio con una velocidad de {self.velMax/10}km/hr")
 
 
 
+carro = automovil("audi", 280)
+carro2 = automovil("BMW",200)
+
+print(f"el automovil {carro.marca} tiene {carro.numLlantas} llantas y tiene una velocidad maxima de {carro.velMax} km/hr")
+carro.avanzar()
+print(f"el automovil {carro2.marca} tiene {carro2.numLlantas} llantas y tiene una velocidad maxima de {carro2.velMax} km/hr")
 
 
