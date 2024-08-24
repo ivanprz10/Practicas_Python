c5524b4dea7e454011ab741f2f73c22dd426e2b4
ivanprz10 <ivanpcruz07@gmail.com>
2024-08-24 14:04:30 -0600

    Proyecto Final

--- Proyecto_final2/main.py
+++ Proyecto_final2/main.py
@@ -1,14 +1,9 @@
 import random
 from diagramas import diagramasAhorcado
 from palabra import palabras
-
-
 palabraAdivinar = random.choice(palabras)
-
 guiones=["-"] * len(palabraAdivinar)
-
 vidas= 7
-
 erradas =[]
 
 while vidas > 0 and "-" in guiones:
@@ -22,7 +17,6 @@ while vidas > 0 and "-" in guiones:
         erradas.append(letra)
         print("esa letra no existe en la palabra :(")
         print(f"letras erradas {erradas}\n")
-        
         vidas -=1
         print(diagramasAhorcado[vidas])
         print(f"vidas restantes = {vidas}")
